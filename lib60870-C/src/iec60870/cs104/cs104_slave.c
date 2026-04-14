7b72bc7c3f6fe0fc668623d0656e21e585b9c99e
sachinkimbal <sachin.sharma@kimbal.io>
2026-04-14 15:00:27 +0530

    temp_update

--- lib60870-C/src/iec60870/cs104/cs104_slave.c
+++ lib60870-C/src/iec60870/cs104/cs104_slave.c
@@ -2703,18 +2703,22 @@ handleASDU(MasterConnection self, CS101_ASDU asdu, CS101_SlavePlugin callingPlug
         break;
     }
 
-    if (isBroadcastCA(slave, ca) == true)
-    {
-        DEBUG_PRINT("CS104_SLAVE: command with broadcast CA not allowed\n");
-
-        responseNegative(asdu, self, CS101_COT_UNKNOWN_CA);
-
-        return true;
-    }
-
     if ((messageHandled == false) && (slave->asduHandler != NULL))
+    {
+        if (isBroadcastCA(slave, ca) == true)
+        {
+            DEBUG_PRINT("CS104_SLAVE: command with broadcast CA not allowed\n");
+
+            responseNegative(asdu, self, CS101_COT_UNKNOWN_CA);
+
+            return true;
+        }
+        
         if (slave->asduHandler(slave->asduHandlerParameter, &(self->iMasterConnection), asdu))
+        {
             messageHandled = true;
+        }
+    }
 
     if (messageHandled == false)
     {
